ab649543f37c8aebad093a50e9d524ccb01ad2f8
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2023-12-11 13:24:57 -0600

    added basic typewriter effect functionality

---
+++ src/Typewriter.java
@@ -0,0 +1,28 @@
+public class Typewriter {
+    private final String s;
+    private String current;
+    private int currentIndex = 0;
+
+    public Typewriter(String s_) {
+        s = s_;
+        current = "";
+    }
+
+    /*
+     * Executed on an interval (e.g. every half second). Types out one more character of the string.
+     */
+    public void pulse() {
+        if (isFinished()) return;
+
+        current += s.charAt(currentIndex);
+        currentIndex++;
+    }
+
+    public String getString() {
+        return current;
+    }
+
+    public boolean isFinished() {
+        return currentIndex >= s.length();
+    }
+}

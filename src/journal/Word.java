2607ca7e6914cd06a6b3c50baf85ae4b1ef7c5a4
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2023-12-14 15:04:16 -0600

    added more functionality to word class
    mostly getters and setters for word info

--- src/journal/Word.java
+++ src/journal/Word.java
@@ -9,13 +9,27 @@ public class Word {
     private final String romanization;
     private final String meaning;
     private final BufferedImage[] spelling;
-    private String meaningGuess;
+    // (sound file for recording of word)
+
+    private String meaningGuess = "";
+    private boolean confirmed = false;
 
     public Word(String romanization, String meaning, CharacterHandler c) {
         this.romanization = romanization;
         this.meaning = meaning;
         this.spelling = Script.getScript(romanization, c);
-        this.meaningGuess = "";
+    }
+
+    public BufferedImage[] getSpelling() {
+        return spelling;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public String getRomanization() {
+        return romanization;
     }
 
     public void setGuess(String guess) {
@@ -24,5 +38,13 @@ public class Word {
 
     public String getGuess() {
         return meaningGuess;
+    }
+
+    public void confirm() {
+        confirmed = true;
+    }
+
+    public boolean getConfirmedStatus() {
+        return confirmed;
     }
 }

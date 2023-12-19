20e8da7b7a9519c2697b387a81828f1adfe3b5f6
agaur12 <117868750+CabbageOfCato@users.noreply.github.com>
2023-12-19 08:44:32 -0600

    Window works!!!!!!!!!!
    :D

--- src/GameHander.java
+++ src/GameHander.java
@@ -4,7 +4,7 @@ import javax.swing.JPanel;
 
 public class GameHander extends JPanel implements Runnable{
     private static final long serialVersionUID = 1L;
-    final int width = 100, height = 100; //may change later
+    final int width = 800, height = 450; //may change later
 	final int fps = 30; // how often the game updates
 
      Thread gameThread;
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,15 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        JFrame window = new JFrame("Game");
+		
+		GameHander gh = new GameHander();
+		window.add(gh);
+		
+		window.pack();
+		window.setVisible(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLocationRelativeTo(null);
+		window.setResizable(true);
     }
 }
